3af72e59838c92b0f718f90bcfd47327b0093e08
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-20 20:16:08 +0330

    ref & fix app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Cart from './pages/cart/cart';
+import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
 import Shop from './pages/shop/shop';
+import Cart from './pages/cart/cart';
 import Nav from './components/nav';
-import { ShopContextProvider } from './context/shopContext';
-
+import {ShopContextProvider} from './context/shopContext'
+import Welcome from './components/welcome';
 function App() {
   return (
     <div className="App">
       <ShopContextProvider>
       <Router>
         <Nav />
+        <Welcome />
         <Routes>
-          <Route path="/" element={<Shop />}/>
-          <Route path="/cart" element={<Cart />}/>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
       </ShopContextProvider>
