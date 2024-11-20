0a781ec65ce874b5797846e24c78f98399f2630a
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-20 16:27:34 +0330

    feat (cart) : complite

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import PRODUCTS from "../../data/products";
-
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../data/products";
+import { ShopContext } from "../../context/shopContext";
+import Product from "../shop/product";
 
 const Cart = () => {
-    
+    const { cartItems } = useContext(ShopContext)
     return (
         <React.Fragment>
             <h1>Your Cart Items</h1>
             <div className="row">
-                {PRODUCTS.map((p)=>{
-
+                {PRODUCTS.map((p) => {
+                    if (cartItems?.some((i) => i.id === p.id && i.count > 0))
+                        return <Product data={p} ></Product>
                 })}
             </div>
         </React.Fragment>
