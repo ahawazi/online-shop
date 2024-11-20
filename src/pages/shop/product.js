d4157334fce31a5ff581a0554d9b10f010de1b18
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-20 15:11:48 +0330

    fix (shop) : don't show the - when we haven't product

--- src/pages/shop/product.js
+++ src/pages/shop/product.js
@@ -3,16 +3,25 @@ import { ShopContext } from "../../context/shopContext";
 
 const Product = (props) => {
   const { id, ProductName, productImage, price } = props.data;
-
-  const {cartItems, addToCart, removeFromCart} = useContext(ShopContext)
+  const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
+  const isInCart = cartItems?.some((item)=>item.id === id)
   return (
     <div className="col-3">
       <img alt="productImage" src={productImage} className="w-100" />
       <h5>{ProductName}</h5>
       <p>price : {price}$</p>
-      <button className="btn btn-info btn-sm" onClick={()=>addToCart(id)}>+</button>
-      <span className="mx-1">{cartItems?.filter((row) => row.id === id)[0]?.count}</span>
-      <button className="btn btn-info btn-sm" onClick={()=>removeFromCart(id)}>-</button>
+      <button className="btn btn-info btn-sm" onClick={() => addToCart(id)}>
+        +
+      </button>
+      <span className="mx-1">
+        {cartItems?.filter((row) => row.id === id)[0]?.count}
+      </span>
+      {isInCart && <button
+        className="btn btn-info btn-sm"
+        onClick={() => removeFromCart(id)}
+      >
+        -
+      </button>}
     </div>
   );
 };
